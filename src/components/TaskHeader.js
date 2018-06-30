80b196c57c3523f9c46ffd0bc3269c5897d0839b
Kyle Cutler <kyle1320@gmail.com>
2018-06-29 22:23:24 -0400

    Add individual task reset buttons, restyle task header

--- src/components/TaskHeader.js
+++ src/components/TaskHeader.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import './TaskHeader.css';
-import { select, deselect, tick, update, deleteTask, addTime, cancelEdit } from '../data/actions';
+import { select, deselect, tick, update, reset, deleteTask, addTime, cancelEdit } from '../data/actions';
 import IconWrapper from './IconWrapper';
-import { faTrashAlt, faPlay, faPause, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt, faPlay, faPause, faPlus, faMinus, faUndo } from '@fortawesome/free-solid-svg-icons'
 
 const mapStateToProps = (state, props) => ({
   isSelected: (state.selectedTask === props.task.id),
@@ -17,6 +17,7 @@ const mapDispatchToProps = (dispatch, props) => ({
   onTick:      ()     => dispatch(tick()),
   onChange:    (data) => dispatch(update(props.task.id, data)),
   onDelete:    ()     => dispatch(deleteTask(props.task.id)),
+  onReset:     ()     => dispatch(reset(props.task.id)),
   onIncrement: ()     => dispatch(addTime(props.task.id, 60000)),
   onDecrement: ()     => dispatch(addTime(props.task.id, -60000)),
   cancelEdit:  ()     => dispatch(cancelEdit())
@@ -91,51 +92,56 @@ class TaskHeader extends Component {
 
     return (
       <div className={`task-header ${this.props.isSelected ? "selected" : ""}`}>
-        <div
-          className="task-header-status"
-          title={this.props.isSelected ? "Deselect Task" : "Select Task"}
-          onClick={this.onToggle} />
-        <div className="task-header-info">
-          <input
-            name="name"
-            ref="name"
-            className="task-name"
-            value={this.props.task.name}
-            onChange={this.onChange}
-            onFocus={this.handleFocus} />
-          <input
-            name="detail"
-            ref="detail"
-            className="task-detail"
-            value={this.props.task.detail}
-            onChange={this.onChange}
-            onFocus={this.handleFocus} />
+        <div className="task-header-center-content">
+          <div className="task-header-info">
+            <div className="task-header-details">
+              <input
+                name="name"
+                ref="name"
+                className="task-name"
+                value={this.props.task.name}
+                onChange={this.onChange}
+                onFocus={this.handleFocus} />
+              <input
+                name="detail"
+                ref="detail"
+                className="task-detail"
+                value={this.props.task.detail}
+                onChange={this.onChange}
+                onFocus={this.handleFocus} />
+            </div>
+            <div className="task-time">
+              {formatTime(this.props.task.time)}
+            </div>
+          </div>
+          <div className="task-header-buttons">
+            <IconWrapper
+              icon={faUndo}
+              onClick={this.props.onReset}
+              title="Reset Task"
+              className="button icon-reset-time" />
+            <IconWrapper
+              icon={faPlus}
+              onClick={this.props.onIncrement}
+              title="Add 1 Minute"
+              className="button icon-plus-time" />
+            <IconWrapper
+              icon={faMinus}
+              onClick={this.props.onDecrement}
+              title="Subtract 1 Minute"
+              className="button icon-minus-time" />
+            <IconWrapper
+              icon={faTrashAlt}
+              onClick={this.props.onDelete}
+              title="Delete Task"
+              className="button icon-trash" />
+          </div>
         </div>
-        <div className="task-time">
-          {formatTime(this.props.task.time)}
-        </div>
-        <div className="task-header-buttons">
-          <IconWrapper
-            icon={faPlus}
-            onClick={this.props.onIncrement}
-            title="Add 1 Minute"
-            className="button icon-plus-time" />
-          <IconWrapper
-            icon={this.props.isSelected ? faPause : faPlay}
-            onClick={this.onToggle}
-            title={this.props.isSelected ? "Deselect Task" : "Select Task"}
-            className="button icon-select-task" />
-          <IconWrapper
-            icon={faMinus}
-            onClick={this.props.onDecrement}
-            title="Subtract 1 Minute"
-            className="button icon-minus-time" />
-          <IconWrapper
-            icon={faTrashAlt}
-            onClick={this.props.onDelete}
-            title="Delete Task"
-            className="button icon-trash" />
-        </div>
+        <IconWrapper
+              icon={this.props.isSelected ? faPause : faPlay}
+              onClick={this.onToggle}
+              title={this.props.isSelected ? "Deselect Task" : "Select Task"}
+              className="task-select-btn" />
       </div>
     );
   }
