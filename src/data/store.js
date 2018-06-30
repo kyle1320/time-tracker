7d4bee48b25c1cbf5814deb28d870c024f11654f
Kyle Cutler <kyle1320@gmail.com>
2018-06-29 20:46:16 -0400

    Refactor to remove saveData.js

--- src/data/store.js
+++ src/data/store.js
@@ -1,6 +1,24 @@
 import { createStore } from 'redux';
 import timeTracker from './timeTracker';
-import { loadState, saveState } from './saveData';
+import { DEFAULT_STATE } from './data-constants';
+
+const STORAGE_KEY = 'savedState';
+
+function loadState() {
+  var state = window.localStorage.getItem(STORAGE_KEY)
+
+  try {
+    state = JSON.parse(state);
+  } catch (e) {
+    state = undefined;
+  }
+
+  return state || DEFAULT_STATE;
+}
+
+function saveState(state) {
+  window.localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+}
 
 const store = createStore(timeTracker, loadState());
 
