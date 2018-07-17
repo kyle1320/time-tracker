448c057db89836874dbaac27b85bdaa40f1facd7
Kyle Cutler <kyle1320@gmail.com>
2018-07-16 20:44:14 -0400

    Remove button tabbability (for now)

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -3,29 +3,13 @@ import React, { Component } from 'react';
 import './Button.css';
 
 export default class Button extends Component {
-  constructor(props) {
-    super(props);
-
-    this.buttonRef = React.createRef();
-  }
-
-  onKeyUp = (event) => {
-    if (event.keyCode === 13 || event.keyCode === 32) {
-      this.buttonRef.current.click();
-      event.preventDefault();
-    }
-  }
-
   render() {
-    var {className, disabled, tabIndex, ...otherProps} = this.props;
+    var {className, disabled, ...otherProps} = this.props;
 
     className = "button " + (disabled ? "disabled " : "") + (className || "");
 
     return (
       <div
-          ref={this.buttonRef}
-          onKeyUp={this.onKeyUp}
-          tabIndex={tabIndex || "0"}
           className={className}
           {...otherProps}>
         {this.props.children}
--- src/components/pressAndHold.js
+++ src/components/pressAndHold.js
@@ -22,9 +22,6 @@ export default function pressAndHold(
 
       this.tickInterval = null;
       this.delayTimeout = null;
-      this.preventClickTimeout = null;
-
-      this.preventClick = false;
     }
 
     tick = (event) => {
@@ -42,20 +39,7 @@ export default function pressAndHold(
         this.delayTimeout = null;
       }
 
-      if (this.preventClickTimeout) {
-        clearTimeout(this.preventClickTimeout);
-        this.preventClickTimeout = null;
-        this.preventClick = false;
-      }
-
-      if (event) {
-        this.preventClick = true;
-        this.preventClickTimeout = setTimeout(() => {
-          this.preventClick = false;
-        }, 100);
-
-        event.preventDefault();
-      }
+      event && event.preventDefault();
     }
 
     start = (event) => {
@@ -74,12 +58,6 @@ export default function pressAndHold(
       }, initialDelay);
     }
 
-    onClick = (event) => {
-      if (!this.preventClick) {
-        this.tick(event);
-      }
-    }
-
     componentWillUnmount() {
       this.stop();
     }
@@ -93,8 +71,6 @@ export default function pressAndHold(
       joinFunc(newProps, 'onTouchStart',  this.start);
       joinFunc(newProps, 'onTouchEnd',    this.stop);
       joinFunc(newProps, 'onTouchCancel', this.stop);
-
-      joinFunc(newProps, 'onClick',       this.onClick);
 
       delete newProps.onTrigger;
 
