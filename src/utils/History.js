725bdf3efcc0c2439515eea771db16b7a7cfb334
Kyle Cutler <kyle1320@gmail.com>
2018-07-29 15:01:01 -0400

    Refactor history

--- src/utils/History.js
+++ src/utils/History.js
@@ -20,18 +20,28 @@ export default class History {
     return state.present;
   }
 
-  // action will be saved in past history, future history will be reset
-  record(state, action) {
+  // Performs the given action on the given state, and returns the new state.
+  // saveEvent:   Whether to save the event in history.
+  // resetFuture: Whether to reset any future history.
+  // isPermanent: Whether this action should be permanent & cannot be undone.
+  //              Has no effect unless saveEvent is true.
+  record(state, action, saveEvent = true, resetFuture = true, isPermanent = false) {
     var {past, present, future} = state;
 
-    past = state.past.concat({
-      state: present,
-      action: action,
-      time: this.currTime++,
-      silent: false
-    });
+    if (saveEvent) {
+      past = state.past.concat({
+        state: present,
+        action: action,
+        time: this.currTime++,
+        permanent: isPermanent
+      });
+    }
+
     present = this.step(present, action);
-    future = [];
+
+    if (resetFuture) {
+      future = [];
+    }
 
     if (past.length > this.limit) {
       past = past.slice(-this.limit);
@@ -42,71 +52,32 @@ export default class History {
 
   // future history will be reset, but action will not be saved in past history
   norecord(state, action) {
-    var {past, present, future} = state;
-
-    present = this.step(present, action);
-    future = [];
-
-    return {past, present, future};
+    return this.record(state, action, false, true);
   }
 
-  // action will be saved, but not undoable.
-  // Does not reset future history.
-  // Useful for permanent actions that have no effect on historical state
+  // action will be permanent, but not reset future history.
   silent(state, action) {
-    var {past, present, future} = state;
-
-    past = state.past.concat({
-      state: present,
-      action: action,
-      time: this.currTime++,
-      silent: true
-    });
-    present = this.step(present, action);
-
-    if (past.length > this.limit) {
-      past = past.slice(-this.limit);
-    }
-
-    return {past, present, future};
+    return this.record(state, action, true, false, true);
   }
 
   // action has no effect on history
   ignore(state, action) {
-    var {past, present, future} = state;
-
-    present = this.step(present, action);
-
-    return {past, present, future};
+    return this.record(state, action, false, false);
   }
 
   // undo a specific event matching the given predicate,
-  // or the last (non-silent) event if no predicate is given
+  // or the last (non-permanent) event if no predicate is given
   undo(state, predicate = () => true) {
     var eventIndex = findLastIndex(state.past, (event) => {
-      return !event.silent && predicate(event.action);
+      return !event.permanent && predicate(event.action);
     });
 
     if (eventIndex < 0) return state;
 
-    var event = state.past[eventIndex];
-    var past = state.past.slice(0, eventIndex);
-    var present = event.state;
-    var future = state.future.concat(event);
-
-    // replay subsequent events
-    for (var i = eventIndex + 1; i < state.past.length; i++) {
-      var action = state.past[i].action;
-
-      past.push({
-        state: present,
-        action: action,
-        time: state.past[i].time,
-        silent: state.past[i].silent
-      });
-
-      present = this.step(present, action);
-    }
+    var event   = state.past[eventIndex];
+    var past    = removeElement(state.past, eventIndex);
+    var present = replay(event.state, past, eventIndex, past.length, this.step);
+    var future  = state.future.concat(event);
 
     return {past, present, future};
   }
@@ -117,33 +88,49 @@ export default class History {
 
     if (eventIndex < 0) return state;
 
-    var event = state.future[eventIndex];
-    var past = state.past.slice();
-    var present = event.state;
-    var future = state.future.slice(0, eventIndex);
-
-    // shuffle the event to the correct place
-    var index = state.past.length;
-    while (index > 0 && event.time < past[index-1].time) {
-      past[index] = past[index - 1];
-      index--;
-    }
-    past[index] = event;
-
-    // replay subsequent events
-    for (var i = index; i < past.length; i++) {
-      var action = past[i].action;
-
-      past[i] = {
-        state: present,
-        action: action,
-        time: past[i].time,
-        silent: past[i].silent
-      };
-
-      present = this.step(present, action);
-    }
+    var event   = state.future[eventIndex];
+    var past    = state.past.slice();
+    var index   = insertChronological(event, past);
+    var present = replay(event.state, past, index, past.length, this.step);
+    var future  = state.future.slice(0, eventIndex);
 
     return {past, present, future};
   }
+}
+
+function removeElement(arr, index) {
+  arr = arr.slice();
+  arr.splice(index, 1);
+  return arr;
+}
+
+// shuffle the event to the correct place
+function insertChronological(event, events) {
+  var index = events.length;
+
+  while (index > 0 && event.time < events[index-1].time) {
+    events[index] = events[index - 1];
+    index--;
+  }
+
+  events[index] = event;
+
+  return index;
+}
+
+function replay(state, events, startIndex, endIndex, step) {
+  for (var i = startIndex; i < endIndex; i++) {
+    var action = events[i].action;
+
+    events[i] = {
+      state: state,
+      action: action,
+      time: events[i].time,
+      permanent: events[i].permanent
+    };
+
+    state = step(state, action);
+  }
+
+  return state;
 }
